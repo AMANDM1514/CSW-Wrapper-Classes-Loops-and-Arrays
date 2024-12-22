f2cbebbc48427a8a76c5d31b827af77cc662370a
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-22 19:22:42 +0530

    Update Q10.java

--- Java1/Q10.java
+++ Java1/Q10.java
@@ -1,4 +1,4 @@
-// Write a program to design a simple calculator (only +,-,*,/ operations). The calculator works as follows:
+// Q10) Write a program to design a simple calculator (only +,-,*,/ operations). The calculator works as follows:
 // Input: ”123+345”
 // Output: Sum=468
 // Input: ”5*10”

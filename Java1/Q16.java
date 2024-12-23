563a565ef9b818a6d8b04f574e2279f6e95c1a27
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-23 22:22:30 +0530

    Update Q16.java

--- Java1/Q16.java
+++ Java1/Q16.java
@@ -1,1 +1,1 @@
-// Write a program to enter the numbers till the user wants and at the end the program should display the largest and smallest numbers entered.
+// Q16) Write a program to enter the numbers till the user wants and at the end the program should display the largest and smallest numbers entered.

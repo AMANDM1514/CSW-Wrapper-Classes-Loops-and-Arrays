4495b074c1317edd8af31b9e3325731d3375ca8f
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-24 23:07:02 +0530

    Update Q4.java

--- Java1/Q4.java
+++ Java1/Q4.java
@@ -1,3 +1,18 @@
 // Q4) Write a program to convert a boolean to a Boolean object.
 // (a) Autoboxing
 // (b) Using Constructor
+
+
+package Assignment_Q;
+
+public class Q4 {
+    public static void main(String[] args) {
+        boolean bool = false;
+
+        Boolean bool1 = bool;
+        System.out.println(bool1);
+        
+        Boolean bool2 = new Boolean(bool);
+        System.out.println(bool2);
+    }
+}

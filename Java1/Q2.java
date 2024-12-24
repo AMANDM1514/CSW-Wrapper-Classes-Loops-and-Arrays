3f4a1d19e9ef7cb2016b94938c8e6293ee72ea5f
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-24 23:05:54 +0530

    Update Q2.java

--- Java1/Q2.java
+++ Java1/Q2.java
@@ -1,3 +1,19 @@
 // Q2) Write a program to convert a float to a Float object.
 // (a) Autoboxing
 // (b) Using Constructor
+
+
+package Assignment_Q;
+
+public class Q2 {
+    public static void main(String[] args) {
+        float b = 45.2f;
+        // [a]
+        Float float1 = b;
+        System.out.println(float1);
+
+        // [b]
+        Float float2 = new Float(b);
+        System.out.println(float2);
+    }
+}

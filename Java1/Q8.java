381f40a7732952e2220d00e7d7d64feb528313f7
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-25 02:26:01 +0530

    Update Q8.java

--- Java1/Q8.java
+++ Java1/Q8.java
@@ -1,2 +1,17 @@
 // Q8) Write a program to read a boolean as a string and convert it to a Boolean object.
 // Explain the concept of converting a base data type to an object type(Wrapping) using the valueOf() method.
+
+package Assignment_Q;
+
+import java.util.Scanner;
+
+public class Q8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        Boolean int1 = Boolean.valueOf(str);
+        System.out.println(int1);
+    }
+}
+

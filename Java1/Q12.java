8b80ae62e6191f6f0bd788df78dad360f077c32a
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-25 02:29:56 +0530

    Update Q12.java

--- Java1/Q12.java
+++ Java1/Q12.java
@@ -1,1 +1,16 @@
 // Q12) Write a program that reads an integer number as a sting and converts it to an int base type.
+
+
+package Assignment_Q;
+
+import java.util.Scanner;
+
+public class Q12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        int num = Integer.parseInt(str);
+        System.out.println(num);
+    }
+}

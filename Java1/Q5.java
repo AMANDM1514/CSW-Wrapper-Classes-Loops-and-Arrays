5d7e0e82a7f0c9ff287cc9f8569af0eb95a3b42c
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-24 23:07:34 +0530

    Update Q5.java

--- Java1/Q5.java
+++ Java1/Q5.java
@@ -1,1 +1,15 @@
 // Q5) Write a program to read an integer as a string and convert it to an Integer object.
+
+package Assignment_Q;
+
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        Integer int1 = Integer.valueOf(str);
+        System.out.println(int1);
+    }
+}

f0c16b71e605fd92f80870bb45d215631e5a4ad5
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-24 23:05:20 +0530

    Update Q1.java

--- Java1/Q1.java
+++ Java1/Q1.java
@@ -1,3 +1,18 @@
 // Q1) Write a program to convert an integer to an Integer object.
 // (a) Autoboxing
 // (b) Using Constructor
+
+package Assignment_Q;
+
+public class Q1 {
+    public static void main(String[] args) {
+        int a = 17;
+        // [a]
+        Integer int1 = a;
+        System.out.println(int1);
+
+        // [b]
+        Integer int2 = new Integer(a);
+        System.out.println(int2);
+    }
+}

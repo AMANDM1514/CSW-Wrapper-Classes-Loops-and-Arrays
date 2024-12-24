3c9e5048647ed829a0fcd6cea87939ffc9ed86c5
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-24 23:08:39 +0530

    Update Q7.java

--- Java1/Q7.java
+++ Java1/Q7.java
@@ -1,1 +1,15 @@
 //Q7) Write a program to read a double as a string and convert it to a Double object.
+
+package Assignment_Q;
+
+import java.util.Scanner;
+
+public class Q7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        Double int1 = Double.valueOf(str);
+        System.out.println(int1);
+    }
+}

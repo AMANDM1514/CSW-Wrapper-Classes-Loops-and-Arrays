4fce25af3c414b6f40229da62068008e48d8cd06
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-24 23:06:25 +0530

    Update Q3.java

--- Java1/Q3.java
+++ Java1/Q3.java
@@ -1,3 +1,17 @@
 // Q3 Write a program to convert a double to a Double object.
 // (a) Autoboxing
 // (b) Using Constructor
+
+
+package Assignment_Q;
+
+public class Q3 {
+    public static void main(String[] args) {
+        double a = 45.23;
+        Double db1 = a;
+        System.out.println(db1);
+
+        Double db2 = new Double(a);
+        System.out.println(db2);
+    }
+}

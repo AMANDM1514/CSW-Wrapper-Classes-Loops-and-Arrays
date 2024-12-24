66a2058185e6b221075d1fc17a3b6d9054d30eb5
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-24 23:08:05 +0530

    Update Q6.java

--- Java1/Q6.java
+++ Java1/Q6.java
@@ -1,1 +1,16 @@
 // Q6) Write a program to read a float as a string and convert it to a Float object.
+
+
+package Assignment_Q;
+
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        Float int1 = Float.valueOf(str);
+        System.out.println(int1);
+    }
+}

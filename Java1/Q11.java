90bd2aade6940f39e173d56373d928dc9c8fef6a
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-25 02:29:12 +0530

    Update Q11.java

--- Java1/Q11.java
+++ Java1/Q11.java
@@ -1,1 +1,16 @@
 // Q11) Write a program that reads a double number as a sting and converts it to a double base type.
+
+
+package Assignment_Q;
+
+import java.util.Scanner;
+
+public class Q11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        double num = Double.parseDouble(str);
+        System.out.println(num);
+    }
+}

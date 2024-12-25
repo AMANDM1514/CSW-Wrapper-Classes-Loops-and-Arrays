c59c7d9c6c3967a3251c52ae55505810def62a37
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-26 00:33:29 +0530

    Update Q13.java

--- Java1/Q13.java
+++ Java1/Q13.java
@@ -1,1 +1,18 @@
 // Q13) Write a program that prompts the user to input a positive integer. It should then print the multiplication table of that number.
+
+
+package Assignment_Q;
+
+import java.util.Scanner;
+
+public class Q13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " * " + i + " = " + (i * n));
+        }
+    }
+}
+

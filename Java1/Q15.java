1e4fd941718e1b30e33ab52f147ac5dd0bbef139
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-26 22:14:14 +0530

    Update Q15.java

--- Java1/Q15.java
+++ Java1/Q15.java
@@ -3,7 +3,7 @@
 
 import java.util.Scanner;
 
-public class SumOfSeries {
+public class Q15 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a positive integer n: ");

6684eb74f289b315f93fd93ce6e79fc487526dd7
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-27 01:09:17 +0530

    Update Q20.java

--- Java1/Q20.java
+++ Java1/Q20.java
@@ -3,7 +3,7 @@
 
 import java.util.Scanner;
 
-public class SortArray {
+public class Q20 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of elements in the array: ");

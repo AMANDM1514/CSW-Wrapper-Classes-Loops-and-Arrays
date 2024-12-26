3de680998d5d2c0ba2db808e43b0050811aa02b2
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-26 22:11:55 +0530

    Update Q15.java

--- Java1/Q15.java
+++ Java1/Q15.java
@@ -1,1 +1,19 @@
 // Q15) Write a program to calculate the sum of following series where n is input by user. 1 + 1/2 + 1/3 + 1/4 + 1/5 +…………1/n
+
+
+import java.util.Scanner;
+
+public class SumOfSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer n: ");
+        int n = scanner.nextInt();
+        double sum = 0.0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += 1.0 / i;
+        }
+
+        System.out.println("Sum of the series: " + sum);
+    }
+}

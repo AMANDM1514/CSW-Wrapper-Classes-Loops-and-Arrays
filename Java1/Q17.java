853a4e1674fd19bbee188ccded6da6d0599ce80f
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-26 22:16:03 +0530

    Update Q17.java

--- Java1/Q17.java
+++ Java1/Q17.java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 
-public class MinMaxInArray {
+public class Q17 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of elements in the array: ");

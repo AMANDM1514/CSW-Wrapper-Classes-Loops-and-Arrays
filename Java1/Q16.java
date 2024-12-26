dddfd22bf346d1e6e734d3714b7ef911c50552b7
AMAN DEV MAHATO <amandevv007@gmail.com>
2024-12-26 22:15:00 +0530

    Update Q16.java

--- Java1/Q16.java
+++ Java1/Q16.java
@@ -3,7 +3,7 @@
 
 import java.util.Scanner;
 
-public class LargestAndSmallest {
+public class Q16 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int largest = Integer.MIN_VALUE;
